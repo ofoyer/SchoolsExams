877cd68089e2faa65b7e41b21e2ea76c5f0c6ce8
Ori-Lenovo <ofoyer@gmai.com>
2017-02-20 16:41:31 +0200

    First Commit. Add MVC before dbContext

---
+++ SchoolExams/src/SchoolExams/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Serialization;
+
+namespace SchoolExams
+{
+    public class Startup
+    {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+            services.AddMvc()
+                    .AddJsonOptions(config =>
+                                    {
+
+                                        config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                                    });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                loggerFactory.AddDebug();
+            }
+
+
+            app.UseStaticFiles();
+
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "defaults",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" }
+
+                    );
+            }); 
+            
+        }
+    }
+}
